514a895294c7a83bc21103212741c12b6862cbd7
DuniaIlmu <127168376+DuniaIlmu@users.noreply.github.com>
2023-10-03 17:57:39 +0700

    Add files via upload

---
+++ mobile/javascript/config.js
@@ -0,0 +1,180 @@
+﻿	var aliasConfig = {
+appName : ["", "", ""],
+totalPageCount : [],
+largePageWidth : [],
+largePageHeight : [],
+normalPath : [],
+largePath : [],
+thumbPath : [],
+
+ToolBarsSettings:[],
+TitleBar:[],
+appLogoIcon:["appLogoIcon"],
+appLogoLinkURL:["appLogoLinkURL"],
+bookTitle : [],
+bookDescription : [],
+ButtonsBar : [],
+ShareButton : [],
+ShareButtonVisible : ["socialShareButtonVisible"],
+ThumbnailsButton : [],
+ThumbnailsButtonVisible : ["enableThumbnail"],
+ZoomButton : [],
+ZoomButtonVisible : ["enableZoomIn"],
+FlashDisplaySettings : [],
+MainBgConfig : [],
+bgBeginColor : ["bgBeginColor"],
+bgEndColor : ["bgEndColor"],
+bgMRotation : ["bgMRotation"],
+backGroundImgURL : ["mainbgImgUrl","innerMainbgImgUrl"],
+pageBackgroundColor : ["pageBackgroundColor"],
+flipshortcutbutton : [],
+BookMargins : [],
+topMargin : [],
+bottomMargin : [],
+leftMargin : [],
+rightMargin : [],
+HTMLControlSettings : [],
+linkconfig : [],
+LinkDownColor : ["linkOverColor"],
+LinkAlpha : ["linkOverColorAlpha"],
+OpenWindow : ["linkOpenedWindow"],
+searchColor : [],
+searchAlpha : [],
+SearchButtonVisible : ["searchButtonVisible"],
+
+productName : [],
+homePage : [],
+enableAutoPlay : ["autoPlayAutoStart"],
+autoPlayDuration : ["autoPlayDuration"],
+autoPlayLoopCount : ["autoPlayLoopCount"],
+BookMarkButtonVisible : [],
+googleAnalyticsID : ["googleAnalyticsID"],
+OriginPageIndex : [],	
+HardPageEnable : ["isHardCover"],	
+UIBaseURL : [],	
+RightToLeft: ["isRightToLeft"],	
+
+LeftShadowWidth : ["leftPageShadowWidth"],	
+LeftShadowAlpha : ["pageShadowAlpha"],
+RightShadowWidth : ["rightPageShadowWidth"],
+RightShadowAlpha : ["pageShadowAlpha"],
+ShortcutButtonHeight : [],	
+ShortcutButtonWidth : [],
+AutoPlayButtonVisible : ["enableAutoPlay"],	
+DownloadButtonVisible : ["enableDownload"],	
+DownloadURL : ["downloadURL"],
+HomeButtonVisible :["homeButtonVisible"],
+HomeURL:['btnHomeURL'],
+BackgroundSoundURL:['bacgroundSoundURL'],
+//TableOfContentButtonVisible:["BookMarkButtonVisible"],
+PrintButtonVisible:["enablePrint"],
+toolbarColor:["mainColor","barColor"],
+loadingBackground:["mainColor","barColor"],
+BackgroundSoundButtonVisible:["enableFlipSound"],
+FlipSound:["enableFlipSound"],
+MiniStyle:["userSmallMode"],
+retainBookCenter:["moveFlipBookToCenter"],
+totalPagesCaption:["totalPageNumberCaptionStr"],
+pageNumberCaption:["pageIndexCaptionStrs"]
+};
+var aliasLanguage={
+frmPrintbtn:["frmPrintCaption"],
+frmPrintall : ["frmPrintPrintAll"],
+frmPrintcurrent : ["frmPrintPrintCurrentPage"],
+frmPrintRange : ["frmPrintPrintRange"],
+frmPrintexample : ["frmPrintExampleCaption"],
+btnLanguage:["btnSwicthLanguage"],
+btnTableOfContent:["btnBookMark"]
+}
+;
+	var bookConfig = {
+	appName:'flippdf',
+	totalPageCount : 0,
+	largePageWidth : 1080,
+	largePageHeight : 1440,
+	normalPath : "files/page/",
+	largePath : "files/large/",
+	thumbPath : "files/thumb/",
+	
+	ToolBarsSettings:"",
+	TitleBar:"",
+	appLogoLinkURL:"",
+	bookTitle:"FLIPBUILDER",
+	bookDescription:"",
+	ButtonsBar:"",
+	ShareButton:"",
+	
+	ThumbnailsButton:"",
+	ThumbnailsButtonVisible:"Show",
+	ZoomButton:"",
+	ZoomButtonVisible:"Yes",
+	FlashDisplaySettings:"",
+	MainBgConfig:"",
+	bgBeginColor:"#cccccc",
+	bgEndColor:"#eeeeee",
+	bgMRotation:45,
+	pageBackgroundColor:"#FFFFFF",
+	flipshortcutbutton:"Show",
+	BookMargins:"",
+	topMargin:10,
+	bottomMargin:10,
+	leftMargin:10,
+	rightMargin:10,
+	HTMLControlSettings:"",
+	linkconfig:"",
+	LinkDownColor:"#808080",
+	LinkAlpha:0.5,
+	OpenWindow:"_Blank",
+
+	BookMarkButtonVisible:'true',
+	productName : 'Demo created by Flip PDF',
+	homePage : 'http://www.flipbuilder.com/',
+	isFlipPdf : "true",
+	TableOfContentButtonVisible:"true",
+	searchTextJS:'javascript/search_config.js',
+	searchPositionJS:undefined
+};
+	
+	
+	;bookConfig.BookTemplateName="metro";bookConfig.loadingCaptionFontSize="20";bookConfig.loadingCaptionColor="#DDDDDD";bookConfig.loadingBackground="#323232";bookConfig.loadingPictureHeight="150";bookConfig.showLoadingGif="Yes";bookConfig.loadingDisplayTime="0";bookConfig.appLogoIcon="files/mobile-ext/appLogoIcon.png";bookConfig.appLogoOpenWindow="Blank";bookConfig.logoHeight="40";bookConfig.logoPadding="0";bookConfig.logoTop="0";bookConfig.toolbarColor="#000000";bookConfig.iconColor="#ECF5FB";bookConfig.pageNumColor="#000000";bookConfig.iconFontColor="#C6C6C6";bookConfig.toolbarAlwaysShow="No";bookConfig.ToolBarVisible="Yes";bookConfig.formFontColor="#FFFFFF";bookConfig.formBackgroundColor="#27181A";bookConfig.ToolBarAlpha="1";bookConfig.CurlingPageCorner="Yes";bookConfig.showBookInstructionOnStart="false";bookConfig.InstructionsButtonVisible="Show";bookConfig.showInstructionOnStart="No";bookConfig.showGotoButtonsAtFirst="No";bookConfig.QRCode="Hide";bookConfig.HomeButtonVisible="Hide";bookConfig.HomeURL="%first page%";bookConfig.aboutButtonVisible="Hide";bookConfig.enablePageBack="Show";bookConfig.ShareButtonVisible="Show";shareObj = [];bookConfig.addCurrentPage="No";bookConfig.EmailButtonVisible="Show";bookConfig.btnShareWithEmailBody="{link}";bookConfig.ThumbnailsButtonVisible="Show";bookConfig.thumbnailColor="#333333";bookConfig.thumbnailAlpha="70";bookConfig.ThumbnailSize="small";bookConfig.BookMarkButtonVisible="Hide";bookConfig.TableOfContentButtonVisible="Show";bookConfig.isHideTabelOfContentNodes="yes";bookConfig.SearchButtonVisible="Show";bookConfig.leastSearchChar="3";bookConfig.searchKeywordFontColor="#FFB000";bookConfig.searchHightlightColor="#ffff00";bookConfig.SelectTextButtonVisible="Show";bookConfig.PrintButtonVisible="Hide";bookConfig.BackgroundSoundButtonVisible="Show";bookConfig.FlipSound="Yes";bookConfig.BackgroundSoundLoop="-1";bookConfig.bgSoundVol="50";bookConfig.AutoPlayButtonVisible="Show";bookConfig.autoPlayAutoStart="No";bookConfig.autoPlayDuration="9";bookConfig.autoPlayLoopCount="1";bookConfig.ZoomButtonVisible="Show";bookConfig.maxZoomWidth="1400";bookConfig.defaultZoomWidth="1000";bookConfig.mouseWheelFlip="Yes";bookConfig.ZoomMapVisible="Hide";bookConfig.DownloadButtonVisible="Hide";bookConfig.PhoneButtonVisible="Hide";bookConfig.AnnotationButtonVisible="Hide";bookConfig.FullscreenButtonVisible="Show";bookConfig.MagnifierButtonVisible="Hide";bookConfig.bgBeginColor="#E2E2E2";bookConfig.bgEndColor="#E2E2E2";bookConfig.bgMRotation="90";bookConfig.backGroundImgURL="files/mobile-ext/backGroundImgURL.jpg";bookConfig.backgroundPosition="stretch";bookConfig.backgroundOpacity="100";bookConfig.backgroundScene="None";bookConfig.LeftShadowWidth="90";bookConfig.LeftShadowAlpha="0.6";bookConfig.RightShadowWidth="55";bookConfig.RightShadowAlpha="0.6";bookConfig.ShowTopLeftShadow="Yes";bookConfig.pageHighlightType="magazine";bookConfig.HardPageEnable="No";bookConfig.hardCoverBorderWidth="8";bookConfig.borderColor="#572F0D";bookConfig.outerCoverBorder="Yes";bookConfig.cornerRound="8";bookConfig.leftMarginOnMobile="0";bookConfig.topMarginOnMobile="0";bookConfig.rightMarginOnMobile="0";bookConfig.bottomMarginOnMobile="0";bookConfig.pageBackgroundColor="#E8E8E8";bookConfig.flipshortcutbutton="Show";bookConfig.phoneFlipShortcutButton="Hide";bookConfig.BindingType="side";bookConfig.RightToLeft="No";bookConfig.FlipDirection="0";bookConfig.flippingTime="0.6";bookConfig.retainBookCenter="Yes";bookConfig.FlipStyle="Flip";bookConfig.autoDoublePage="Yes";bookConfig.isTheBookOpen="No";bookConfig.DoubleSinglePageButtonVisible="hide";bookConfig.thicknessWidthType="Thinner";bookConfig.thicknessColor="#ffffff";bookConfig.SingleModeBanFlipToLastPage="No";bookConfig.showThicknessOnMobile="No";bookConfig.isSingleBookFullWindowOnMobile="no";bookConfig.isStopMouseMenu="yes";bookConfig.restorePageVisible="No";bookConfig.topMargin="10";bookConfig.bottomMargin="10";bookConfig.leftMargin="10";bookConfig.rightMargin="10";bookConfig.hideMiniFullscreen="no";bookConfig.maxWidthToSmallMode="400";bookConfig.maxHeightToSmallMode="300";bookConfig.leftRightPnlShowOption="None";bookConfig.highDefinitionConversion="yes";bookConfig.LargeLogoPosition="top-left";bookConfig.LargeLogoTarget="Blank";bookConfig.isFixLogoSize="No";bookConfig.logoFixWidth="0";bookConfig.logoFixHeight="0";bookConfig.SupportOperatePageZoom="Yes";bookConfig.showHelpContentAtFirst="No";bookConfig.updateURLForPage="No";bookConfig.passwordTips="Please contact the <a href=mailto:author@sample.com><u>author</u></a> to access the web";bookConfig.OnlyOpenInIframe="No";bookConfig.OnlyOpenInIframeInfo="No reading rights";bookConfig.OpenWindow="Blank";bookConfig.showLinkHint="No";bookConfig.MidBgColor="#516522";bookConfig.useTheAliCloudChart ="no";bookConfig.totalPageCount=16;bookConfig.largePageWidth=1200;bookConfig.largePageHeight=1699;;bookConfig.securityType="1";bookConfig.CreatedTime ="231003175227";bookConfig.bookTitle="Modul Energi Listrik";bookConfig.bookmarkCR="f731ff51e2dfae751dfcc34d1c06480557676f31";bookConfig.productName="Flip PDF Professional";bookConfig.homePage="http://www.flipbuilder.com";bookConfig.searchPositionJS="mobile/javascript/text_position[1].js";bookConfig.searchTextJS="mobile/javascript/search_config.js";bookConfig.normalPath="files/mobile/";bookConfig.largePath="files/mobile/";bookConfig.thumbPath="files/thumb/";bookConfig.userListPath="files/extfiles/users.js";bookConfig.UIBaseURL='mobile/';var language = [];;function orgt(s){ return binl2hex(core_hx(str2binl(s), s.length * chrsz));};; var pageEditor = {"setting":{"annoPlaying":"true","shoppingCartHTML":"false","shoppingCartOptinon":{"type":"PayPal","paypal":"","method":"POST","sandbox":"false","address":"","theme":"","body":"Hi xxx     I'm going to buy below product(s):      ${shopping}  Full Name","showPrice":"true","showTime":"true"}}, "pageAnnos":[[],[],[],[],[],[],[],[],[],[{"annotype":"com.mobiano.flipbook.pageeditor.TAnnoLink","annoId":"202331747456323","alpha":"1","overColor":"8388736","downColor":"8388736","outColor":"11184810","overAlpha":"0.2","downAlpha":"0.2","outAlpha":"0","pageViewedBool":"true","ellipseH":"0","ellipseW":"0","location":{"tannoName":"link1","x":"0.363795","y":"0.791408","width":"0.272059","height":"0.036479","rotation":"0","reflection":"false","reflectionType":"0","reflectionAlpha":"0","pageWidth":"594.96","pageHeight":"842.25"},"hint":{"hintShapeColor":"0","hintShapeColor2":"0","hintShapeAlpha":"0","hintW":"0","hintH":"0","hintAuto":"true","hintShapeType":"2","text":""},"shadow":{"hasDropShadow":"false","shadowDistance":"0","shadowAngle":"0","shadowColor":"0","shadowAlpha":"0","shadowBlurX":"0","shadowBlurY":"0"},"highlightsBool":"false","linkStatus":"1","mouseOver":"false","borderColor":"16737792","action":{"triggerEventType":"mouseDown","actionType":"com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL","url":"https://app.wizer.me/learn/8H5HZX","linkTarget":"Blank"}},{"annotype":"com.mobiano.flipbook.pageeditor.TAnnoMovie","annoId":"202331747452744","alpha":"1","prompt":"","promptTime":"","moviePlayTriggerEvt":"none","movieStopTriggerEvt":"EVT_PageOffView","replayWhenPlayEventTrigger":"0","location":{"tannoName":"movie1","x":"0.11765496840123706","y":"0.1495992876224399","width":"0.7632445878714536","height":"0.3395666369842683","rotation":"0","reflection":"false","reflectionType":"0","reflectionAlpha":"0","pageWidth":"594.96","pageHeight":"842.25"},"hint":{"hintShapeColor":"0","hintShapeColor2":"8388736","hintShapeAlpha":"1","hintW":"0","hintH":"0","hintAuto":"true","hintShapeType":"2","text":""},"shadow":{"hasDropShadow":"false","shadowDistance":"4","shadowAngle":"270","shadowColor":"0","shadowAlpha":"0.6","shadowBlurX":"4","shadowBlurY":"4"},"cpName":"fbp","className":"com.mobiano.flipbook.movie.TAnnoMovie","H5Replay":"false","H5PlaybackNumber":"0","url":"files/pageConfig/y2mate.com - MacamMacam Pembangkit Listrik Sebagai Sumber Energi Alternatif_720p.mp4","skinColor":"10066329","skin":"Skin 1","sizeBool":"false","previewUrl":"","isShowTimer":"false"}],[],[{"annotype":"com.mobiano.flipbook.pageeditor.TAnnoMovie","annoId":"202331747456725","alpha":"1","prompt":"","promptTime":"","moviePlayTriggerEvt":"none","movieStopTriggerEvt":"EVT_PageOffView","replayWhenPlayEventTrigger":"0","location":{"tannoName":"movie2","x":"0.10925104208686298","y":"0.13891362422083706","width":"0.7904733091300256","height":"0.3110715345799941","rotation":"0","reflection":"false","reflectionType":"0","reflectionAlpha":"0","pageWidth":"594.96","pageHeight":"842.25"},"hint":{"hintShapeColor":"0","hintShapeColor2":"8388736","hintShapeAlpha":"1","hintW":"0","hintH":"0","hintAuto":"true","hintShapeType":"2","text":""},"shadow":{"hasDropShadow":"false","shadowDistance":"4","shadowAngle":"270","shadowColor":"0","shadowAlpha":"0.6","shadowBlurX":"4","shadowBlurY":"4"},"cpName":"fbp","className":"com.mobiano.flipbook.movie.TAnnoMovie","H5Replay":"false","H5PlaybackNumber":"0","url":"files/pageConfig/y2mate.com - Cara Kerja Pembangkit Listrik Tenaga Air_480p.mp4","skinColor":"10066329","skin":"Skin 1","sizeBool":"false","previewUrl":"","isShowTimer":"false"}],[],[{"annotype":"com.mobiano.flipbook.pageeditor.TAnnoMovie","annoId":"202331747455655","alpha":"1","prompt":"","promptTime":"","moviePlayTriggerEvt":"none","movieStopTriggerEvt":"EVT_PageOffView","replayWhenPlayEventTrigger":"0","location":{"tannoName":"movie3","x":"0.10757025682398816","y":"0.5188483229444939","width":"0.7939189189189189","height":"0.31588008311071536","rotation":"0","reflection":"false","reflectionType":"0","reflectionAlpha":"0","pageWidth":"594.96","pageHeight":"842.25"},"hint":{"hintShapeColor":"0","hintShapeColor2":"8388736","hintShapeAlpha":"1","hintW":"0","hintH":"0","hintAuto":"true","hintShapeType":"2","text":""},"shadow":{"hasDropShadow":"false","shadowDistance":"4","shadowAngle":"270","shadowColor":"0","shadowAlpha":"0.6","shadowBlurX":"4","shadowBlurY":"4"},"cpName":"fbp","className":"com.mobiano.flipbook.movie.TAnnoMovie","H5Replay":"false","H5PlaybackNumber":"0","url":"files/pageConfig/y2mate.com - KENAPA LISTRIK PADAM_1080p.mp4","skinColor":"10066329","skin":"Skin 1","sizeBool":"false","previewUrl":"","isShowTimer":"false"}],[{"annotype":"com.mobiano.flipbook.pageeditor.TAnnoLink","annoId":"202331747451001","alpha":"1","overColor":"8388736","downColor":"8388736","outColor":"11184810","overAlpha":"0.2","downAlpha":"0.2","outAlpha":"0","pageViewedBool":"true","ellipseH":"0","ellipseW":"0","location":{"tannoName":"","x":"0.357449","y":"0.8608610000000001","width":"0.284654","height":"0.036479","rotation":"0","reflection":"false","reflectionType":"0","reflectionAlpha":"0","pageWidth":"594.96","pageHeight":"842.25"},"hint":{"hintShapeColor":"0","hintShapeColor2":"0","hintShapeAlpha":"0","hintW":"0","hintH":"0","hintAuto":"true","hintShapeType":"2","text":""},"shadow":{"hasDropShadow":"false","shadowDistance":"0","shadowAngle":"0","shadowColor":"0","shadowAlpha":"0","shadowBlurX":"0","shadowBlurY":"0"},"highlightsBool":"false","linkStatus":"1","mouseOver":"false","borderColor":"16737792","action":{"triggerEventType":"mouseDown","actionType":"com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL","url":"https://docs.google.com/forms/d/e/1FAIpQLSeNSpXJIwB668e77taKYspicAu0TAVRsnFRBVsFf2ppk9VFEg/viewform?usp=sf_link","linkTarget":"Blank"}}],[{"annotype":"com.mobiano.flipbook.pageeditor.TAnnoLink","annoId":"202331747456028","alpha":"1","overColor":"8388736","downColor":"8388736","outColor":"11184810","overAlpha":"0.2","downAlpha":"0.2","outAlpha":"0","pageViewedBool":"true","ellipseH":"0","ellipseW":"0","location":{"tannoName":"","x":"0.38419499999999995","y":"0.514452","width":"0.231933","height":"0.032055","rotation":"0","reflection":"false","reflectionType":"0","reflectionAlpha":"0","pageWidth":"594.96","pageHeight":"842.25"},"hint":{"hintShapeColor":"0","hintShapeColor2":"0","hintShapeAlpha":"0","hintW":"0","hintH":"0","hintAuto":"true","hintShapeType":"2","text":""},"shadow":{"hasDropShadow":"false","shadowDistance":"0","shadowAngle":"0","shadowColor":"0","shadowAlpha":"0","shadowBlurX":"0","shadowBlurY":"0"},"highlightsBool":"false","linkStatus":"1","mouseOver":"false","borderColor":"16737792","action":{"triggerEventType":"mouseDown","actionType":"com.mobiano.flipbook.pageeditor.TAnnoActionOpenURL","url":"https://duniailmu.github.io/tes1_menghasilkanlistrik/","linkTarget":"Blank"}}]]}; bookConfig.isFlipPdf=false; var pages_information =[{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"},{pageColor:"16777215",pageIsStrech:"no"}];	
+	if(language&&language.length>0&&language[0]&&language[0].language){
+		bookConfig.language=language[0].language;
+	}
+	
+try{
+	for(var i=0;pageEditor!=undefined&&i<pageEditor.length;i++){
+		if(pageEditor[i].length==0){
+			continue;
+		}
+		for(var j=0;j<pageEditor[i].length;j++){
+			var anno=pageEditor[i][j];
+			if(anno==undefined)continue;
+			if(anno.overAlpha==undefined){
+				anno.overAlpha=bookConfig.LinkAlpha;
+			}
+			if(anno.outAlpha==undefined){
+				anno.outAlpha=0;
+			}
+			if(anno.downAlpha==undefined){
+				anno.downAlpha=bookConfig.LinkAlpha;
+			}
+			if(anno.overColor==undefined){
+				anno.overColor=bookConfig.LinkDownColor;
+			}
+			if(anno.downColor==undefined){
+				anno.downColor=bookConfig.LinkDownColor;
+			}
+			if(anno.outColor==undefined){
+				anno.outColor=bookConfig.LinkDownColor;
+			}
+			if(anno.annotype=='com.mobiano.flipbook.pageeditor.TAnnoLink'){
+				anno.alpha=bookConfig.LinkAlpha;
+			}
+		}
+	}
+}catch(e){
+}
+try{
+	$.browser.device = 2;
+}catch(ee){
+}
